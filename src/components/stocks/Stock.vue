3880903048d2b03c53416eb761c004ff2f193127
Giovane Aguiar <giovaneaguiar@ice.ufjf.br>
2021-05-14 12:49:13 -0300

    Pré-finaliza Stock.vue (Ações).

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>Ações</h1>
+  <!-- padding right 3 e padding bottom 3, ou seja, 
+  faz o espaçamento entre os blocos de açoes -->
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <!-- xs12 md6 lg4 = atributos para melhorar a visualização em
+    dispositivos pequenos, médios e computadores convencionais
+    xs12 - celular / md6 - tablet / lg4 - desktop -->
+    <v-card class="green darken-3 white--text">
+      <!-- v-card = título desse componente-->
+      <v-card-title class="headline">
+        <strong>NOME<small> (Preço: PRECO)</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <!-- outro v-card = corpo do componente -->
+      <!-- colocar aqui a quantidade que o usuario vai 
+      selecionar e o botao para comprar uma ação -->
+      <v-container fill-height>
+        <!-- fill-height = preencher toda altura -->
+        <v-text-field label="Quantidade" type="number" />
+        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,13 +1,26 @@
 <template>
-  <h1>Ações Cadastradas</h1>
+  <v-layout row wrap> 
+    <!-- pega o array definido em data() -->
+    <Stock v-for="stock in stocks" :key="stock.id" />
+  </v-layout>
 </template>
 
 <script>
+import Stock from "./Stock";
 export default {
-
-}
+  components: { Stock },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 280 },
+        { id: 4, name: "Twitter", price: 83 },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
