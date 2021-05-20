427e545e5b4fbb78fafb4e711539b22acb8e714a
Giovane <giovaneaguiar@ice.ufjf.br>
2021-05-20 12:17:45 -0300

    Finaliza animações nas rotas, com @keyframes.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
     <Header />
     <v-content>
       <v-container>
+        <transition name="slide" mode="out-in">
+          <!-- transição das rotas -->
         <router-view></router-view>
+        </transition>
       </v-container>
     </v-content>
   </v-app>
@@ -17,11 +20,32 @@ export default {
 
   created() {
     //chamar action do módulo
-    this.$store.dispatch('initStocks')
-  }
+    this.$store.dispatch("initStocks");
+  },
 };
 </script>
 
 <style>
+/* animações = de onde eu vou sair, para onde eu vou chegar */
+      @keyframes slide-in {
+            from { transform: translateY(-30px); opacity: 0; }
+           to { transform: translateY(0px); opacity: 1; }
+      }
+
+      @keyframes slide-out {
+        from { transform: translateY(0px); opacity: 1; }
+           to { transform: translateY(-30px); opacity: 0; } 
+
+      }
+
+      .slide-enter-active {
+        animation: slide-in 0.3s ease;
+        /* propriedade animation, qual keyframe vai usar,
+        o tempo, e qual algoritmo vai usar para fazer a animação */
+      }
+
+      .slide-leave-active {
+        animation: slide-out 0.3s ease;
+      }
 </style>
 
